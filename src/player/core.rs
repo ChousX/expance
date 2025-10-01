11a91e9d349a1bcad29c69a01e1f9d5defa31b7c
ChousX <gmanenergy@gmail.com>
2025-10-01 08:58:03 -0400

    fixed braking around player core

--- src/player/core.rs
+++ src/player/core.rs
@@ -9,7 +9,7 @@ use crate::{
 pub struct PlayerCorePlugin;
 impl Plugin for PlayerCorePlugin {
     fn build(&self, app: &mut App) {
-        app.add_observer(spawn_player_core);
+        app.add_observer(spawn_player_core).add_observer(brake_tiles_around_core);
     }
 }
 
@@ -21,24 +21,36 @@ pub struct PlayerCoreSprite {
     #[asset(path = "placeholder/Diamond/Sprite-0001.png")]
     pub default: Handle<Image>,
 }
+
 fn spawn_player_core(
     trigger: Trigger<OnAdd, Player>,
     mut commands: Commands,
     sprite_texture: Res<PlayerCoreSprite>,
-    mut out: EventWriter<BrakeTile>,
 ) {
     info!("spawn player core");
     let sprite = Sprite::from_image(sprite_texture.default.clone());
     let transform = Transform::from_xyz(0.0, 0.0, 1.0);
     commands.spawn((PlayerCore, transform, sprite, OwnedBy(trigger.target())));
+}
 
+fn brake_tiles_around_core(
+    trigger: Trigger<OnAdd, PlayerCore>,
+    mut out: EventWriter<BrakeTile>,
+    transform: Query<&GlobalTransform>,
+) {
+    let Ok(transform) = transform.get(trigger.target()) else {
+        return;
+    };
+    let translation = transform.translation();
+    let offset = translation.xy();
+    let z = translation.z;
     //Brake all tiles around core
     for x in -1..=1 {
         for y in -1..=1 {
             out.write(BrakeTile::ByPos(vec3(
-                x as f32 * TILE_SIZE.x,
-                y as f32 * TILE_SIZE.y,
-                0.0,
+                x as f32 * TILE_SIZE.x + offset.x,
+                y as f32 * TILE_SIZE.y + offset.y,
+                z,
             )));
         }
     }
